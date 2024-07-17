a0bc3fd6fc68dde735057524089efdedbcf90506
reine-work <62882259+reine-work@users.noreply.github.com>
2024-07-17 10:11:57 +0800

    Update horizontal-div.js

--- js/horizontal-div.js
+++ js/horizontal-div.js
@@ -1,44 +1,66 @@
-(function(){
-    init();
 
-    var g_containerInViewport;
-    function init(){
-        setStickyContainersSize();
-        bindEvents();
-    }
 
-    function bindEvents(){
-        window.addEventListener("wheel", wheelHandler);        
-    }
 
-    function setStickyContainersSize(){
-        document.querySelectorAll('.sticky-container').forEach(function(container){
-            const stikyContainerHeight = container.querySelector('.hori-box').scrollWidth;
-            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
-        });
-    }
-
-    function isElementInViewport (el) {
-        const rect = el.getBoundingClientRect();
-        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-    }
-
-    function wheelHandler(evt){
-        
-        const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
-            return isElementInViewport(container);
-        })[0];
-
-        if(!containerInViewPort){
-            return;
+(function() {
+    (function(){
+        init();
+    
+        var g_containerInViewport;
+        function init(){
+            setStickyContainersSize();
+            bindEvents();
         }
-
-        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
-        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
-        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
-
-        if(g_canScrollHorizontally){
-            containerInViewPort.querySelector('.hori-box').scrollLeft += evt.deltaY;
+    
+        function bindEvents(){
+            window.addEventListener("wheel", wheelHandler);        
         }
+    
+        function setStickyContainersSize(){
+            document.querySelectorAll('.sticky-container').forEach(function(container){
+                const stikyContainerHeight = container.querySelector('.hori-box').scrollWidth;
+                container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+            });
+        }
+    
+        function isElementInViewport (el) {
+            const rect = el.getBoundingClientRect();
+            return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+        }
+    
+        function wheelHandler(evt){
+            
+            const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
+                return isElementInViewport(container);
+            })[0];
+    
+            if(!containerInViewPort){
+                return;
+            }
+    
+            var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+            var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+            let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+    
+            if(g_canScrollHorizontally){
+                containerInViewPort.querySelector('.hori-box').scrollLeft += evt.deltaY;
+            }
+        }
+    })();
+  
+    // Make g_canScrollHorizontally accessible outside wheelHandler (if needed)
+    let g_canScrollHorizontally = false;
+  
+    function init() {
+      setStickyContainersSize();
+      bindEvents();
     }
-})();
+  
+    function bindEvents() {
+      window.addEventListener("wheel", wheelHandler);
+    }
+  
+    // ... rest of your code
+  
+    document.addEventListener('DOMContentLoaded', init);
+  })();
+  
