87e0fc3961d674dc541d95252f25b608e8b762a2
reine-work <62882259+reine-work@users.noreply.github.com>
2024-09-04 08:12:38 +0800

    Fixed navbar

--- js/site.js
+++ js/site.js
@@ -155,3 +155,16 @@ MySite.HorizontalScroller = (function() {
 
 // Initialize the AJAX loader when the page loads
 MySite.AjaxLoader.init();
+
+const hamburger = document.querySelector(".hamburger")
+const nav_menu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  nav_menu.classList.remove("active");
+}))
